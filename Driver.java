57d88e3c233f28e5a092aac643d25c9716fac697
Barış Şenyerli <58127640+BarisX@users.noreply.github.com>
2019-12-03 03:40:11 +0300

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package antcolony;
+
+/**
+ *
+ * @author Barış Şenyerli
+ */
+public class Driver {
+    static final int NUMBER_OF_ANTS = 500;
+    static ExecutorService executorService = Executors.newFixedThread
+
+}

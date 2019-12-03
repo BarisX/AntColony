79ad17e20e130ff90d19163dfd34f53ed19253c5
Barış Şenyerli <58127640+BarisX@users.noreply.github.com>
2019-12-03 03:39:37 +0300

    Create ant.java

---
+++ ant.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package antcolony;
+
+/**
+ *
+ * @author Barış Şenyerli
+ */
+public class Ant implements Callable<Ant>{
+    @Override
+    public Ant call() throws Exception {
+        return null;
+    }
+
+}
